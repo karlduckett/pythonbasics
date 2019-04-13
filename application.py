a218e0417a26f6bf4429bbcb9d44ffd94a56d6db
Karl Duckett <karlplduckett@gmail.com>
2019-04-13 19:56:09 +1200

    Update application.py
    App run added

--- application.py
+++ application.py
@@ -1,6 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/')
+def hello_world():
+   return 'Hello World’
+
+if __name__ == '__main__':
+   app.run()
